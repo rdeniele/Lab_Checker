d46a2e663b1f61ad36030a3d9d1df075a86a13c3
rdeniele <rondenielep13@gmail.com>
2023-12-06 12:54:47 +0800

    Fourth commit: added addlav.css for css of Adding laboratories

--- addLab.php
+++ addLab.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Update Laboratory</title>
-    <link rel="stylesheet" href="updatelab.css">
+    <title>Add Laboratory</title>
+    <link rel="stylesheet" href="addlav.css">
 
 </head>
 <body>
+
+<div class="container">
+
+<div class="box">
     <h1>ADD LABORATORY</h1>
     <form name="form1" method="post" action="addLab_confirm.php">
         <!-- lab name-->
@@ -20,9 +24,15 @@
         <label for="desc">Description:</label>
         <input type="text" name="desc" value=""><br>
         <!-- submit button -->
-        <button type="add" id="update_button" name="add">Add</button> 
+        <div class="box2">
+            <button type="add" id="addbutton" name="add">Add</button> 
 
+            </div>
+
+    </form>
+
+    </div>
+
+    </div>
 </body>
 </html>
-
-
